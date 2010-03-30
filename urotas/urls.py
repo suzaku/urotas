3e725280ea3f5de765fabb67c324beef59e67946
Satoru Logic <SatoruLogic@kingsoft.com>
2010-03-30 17:10:47 +0800

    ??url??

--- urotas/urls.py
+++ urotas/urls.py
@@ -1,17 +1,22 @@
+import os
+
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^urotas/', include('urotas.foo.urls')),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^note/', include('urotas.note.urls')),
+
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': os.path.join(os.path.dirname(__file__), 'static')}),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
