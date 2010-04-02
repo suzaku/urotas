3e8adf1c7ed8c7f6b1818feb980d7943fa2acb5c
Satoru Logic <SatoruLogic@kingsoft.com>
2010-04-02 22:32:02 +0800

    ?django.forms??timestamp?delta?????

--- urotas/note/forms.py
+++ urotas/note/forms.py
@@ -2,3 +2,8 @@ from django import forms
 
 class NoteForm(forms.Form):
     content = forms.CharField(max_length=128)
+
+class QueryNotesByTimeForm(forms.Form):
+    timestamp = forms.DateTimeField(input_formats=['%Y-%m-%d %H:%M:%S.%f'],
+                                    required=True)
+    delta = forms.IntegerField(initial=10, min_value=1, required=False)
