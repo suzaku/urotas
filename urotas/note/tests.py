3646f7f68d6e993b700c47c09567f3784d66eaf1
Satoru Logic <SatoruLogic@kingsoft.com>
2010-04-03 17:22:28 +0800

    ??????????????, ???????????

--- urotas/note/tests.py
+++ urotas/note/tests.py
@@ -7,17 +7,15 @@ Replace these with more appropriate tests for your application.
 
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from utils import tag_linkify
+class NoteTest(TestCase):
+    def setUp(self):
+        self.content_without_tag = 'nothing about tag'
+        self.content = '#Tag:># message #Tag2#'
+        self.linkified_content = '<a href="/note/search?tag=Tag%3A%3E">#Tag:&gt;#</a> message <a href="/note/search?tag=Tag2">#Tag2#</a>'
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+    def test_note_tag_to_link(self):
+        self.assertEqual(self.content_without_tag,
+                         tag_linkify(self.content_without_tag))
+        self.assertEqual(self.linkified_content,
+                         tag_linkify(self.content))
---
+++ urotas/note/utils.py
@@ -0,0 +1,26 @@
+#-*-coding=utf-8-*-
+
+from urllib import quote
+import itertools
+
+from django.conf import settings
+from django.utils.html import escape
+
+import re
+TAG_PATTERN = re.compile(r'#[^#]+#')
+
+def tag_linkify(content):
+    untagged_tokens = TAG_PATTERN.split(content)
+    tagged_tokens = TAG_PATTERN.findall(content)
+    if tagged_tokens:
+        buf = []
+        for untagged, tagged in itertools.izip_longest(untagged_tokens,
+                                           tagged_tokens, fillvalue=''):
+            if untagged:
+                buf.append(escape(untagged))
+            if tagged:
+                buf.append('<a href="/note/search?tag=%s">%s</a>' % (
+                                        quote(tagged[1:-1]), escape(tagged) ))
+        return ''.join(buf)
+    else:
+        return escape(content)
