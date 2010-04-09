051bfcf3c320156cca97f3064e8f024e6fe183a6
Satoru Logic <SatoruLogic@gmail.com>
2010-04-09 15:40:41 +0800

    loadOnScroll add support for setting load_url and params

--- urotas/static/scripts/urotas.note.js
+++ urotas/static/scripts/urotas.note.js
@@ -8,13 +8,23 @@ function updateNoteItem(elm, note) {
     return elm;
 }
 
-function loadOnScroll() {
+function loadOnScroll(options) {
+    // TODO add support for setting params
+    var defaults = {
+        load_url: '/note/list/json',
+        params: {}
+    };
+
+    var options = $.extend(defaults, options);
+
     $(window).scroll(function(){
         var lowestTop = $(document).height() - $(window).height();
         if ($(window).scrollTop() == lowestTop) {
             var last_node = $('.messages .note:last');
             var timestamp = last_node.attr('timestamp');
-            $.getJSON('/note/list', {timestamp: timestamp}, function(notes){
+            var params = $.extend(options.params, 
+                                 {timestamp: timestamp});
+            $.getJSON(options.load_url, params, function(notes){
                 if (notes) {
                     var note_template = $("#note_template");
                     $.each(notes, function(i){
